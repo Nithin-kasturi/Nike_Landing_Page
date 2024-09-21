e97169433515473c9239c8dc1ad1f81be11a96cb
suryateja <bsuryateja777@gmail.com>
2024-09-21 12:16:21 +0530

    Surya change

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -11,7 +11,7 @@ const PopularProducts = () => {
         <h2 className='font-palanquin text-4xl font-bold'>
           Our <span className='text-coral-red'>Popular</span> products</h2>
         <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
-          Experience top-notch quality and style with our sought-after selections.Discover a world of comfort,design,and value.
+          Lanj top-notch quality and style with our sought-after selections.Discover a world of comfort,design,and value.
         </p>
       </div>
       <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
